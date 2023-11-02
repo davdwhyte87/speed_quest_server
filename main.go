d38c7d5077f115b88a089a6bd1ac409188aa7d6c
vhenngames <37925345+davdwhyte87@users.noreply.github.com>
2023-11-02 09:34:51 +0100

    Update main.go

--- main.go
+++ main.go
@@ -21,6 +21,7 @@ func main() {
 	utils.InitLogger()
 
 	router := gin.Default()
+	router.Use(CORSMiddleware())
 	router.GET("/", func(ctx *gin.Context) {
 		ctx.JSON(200, gin.H{"data": "Armorgens API running ..."})
 	})
@@ -36,6 +37,26 @@ func main() {
 	// routes setup
 	routes.Routes(router)
 	routes.VisaRoutes(router)
+
 	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
 	router.Run("localhost:" + port)
+}
+
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
+	}
 }
